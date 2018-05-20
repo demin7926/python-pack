eb3c2a01dc9a93982c78069b49431ed23466fcf6
Mackee <demin7926@gmail.com>
2018-05-20 15:36:31 +0800

    new testing

--- HPSAIP/celery/distributed-tasks/src/worker1/task1.py
+++ HPSAIP/celery/distributed-tasks/src/worker1/task1.py
@@ -2,11 +2,14 @@
 
 import os, subprocess
 from celery import Celery
+from kombu.common import Broadcast
 
 app = Celery('DTQDemo', broker='amqp://rabbitmq')
+app.conf.task_queues = (Broadcast('BroadcastTask'),)
+app.conf.task_routes = {'my.t1': {'queue': 'BroadcastTask'}}
 
-#@app.task(name="mytask1")
-def task2_func():
+#@app.task(name="my.t1")
+def task_func():
     cwd = os.path.dirname(os.path.realpath(__file__))
     cmd = os.path.join(cwd, "task1.sh")
     print("worker1.task_func() start: run command %s" % cmd)
@@ -16,8 +19,8 @@ def task2_func():
     print("worker1.task_func() finish: [%d]" % retcode)
     return retcode
 
-task2_func = app.task(name="mytask1")(task2_func)
 
+task_func = app.task(name="my.t1")(task_func)
 
 argv = ['worker', '--loglevel=INFO']
 app.worker_main(argv)
--- HPSAIP/celery/distributed-tasks/src/worker2/task2.py
+++ HPSAIP/celery/distributed-tasks/src/worker2/task2.py
@@ -2,11 +2,14 @@
 
 import os, subprocess
 from celery import Celery
+from kombu.common import Broadcast
 
 app = Celery('DTQDemo', broker='amqp://rabbitmq')
+app.conf.task_queues = (Broadcast('BroadcastTask'),)
+app.conf.task_routes = {'my.t2': {'queue': 'BroadcastTask'}}
 
-@app.task(name="mytask2")
-def task2_func():
+@app.task(name="my.t2")
+def task_func():
     cwd = os.path.dirname(os.path.realpath(__file__))
     cmd = os.path.join(cwd, "task2.sh")
     print("worker2.task_func() start: run command %s" % cmd)
